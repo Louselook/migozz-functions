e96d6fc3f253e258cbab9b07cf0d47c92eefc12f
JuanEs Arenilla Buendia <juanes.arenilla@gmail.com>
2025-09-21 15:48:39 -0500

    # Fecha: [21/09/2025] ## 1. feactur/register     - Se modificaron los modulos de chat y user_deetails     - Flujo: Ahora el flujo es acorde a lo esperado con navigation entre pantallas     - Modificacion: se agrego el parametron 'pageIndicator' en more_user_details.dart para controlar la pagina visible del page.view     - Se creo el boton newButtonBack en button_social.dart para volver al back     - en ia_chat_scren.dart se agrego la navegacion por acciones segun el pageIndicator junto con .then para controlar acciones al volver     - Pendiente: Refactor de modularizar los cambios realizados en ia_chat_scren.dart
    Merge: integración de cambios remotos (main) con mis cambios locales

--- lib/features/profile/service/profile_service.dart
+++ lib/features/profile/service/profile_service.dart
@@ -1,6 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/material.dart';
-import 'package:migozz_app/core/color.dart';
 
 class ProfileService {
   final _db = FirebaseFirestore.instance;
