53a248acb55332243eebde7a48ad7574dcfa5c4f
GiovannyGil <GiovannyDev01@gmail.com>
2025-10-03 08:05:49 -0500

    mostrar datos en profile
    se muestran los datos de "username" y "displayName" en el profile
    se agrega icono provicional a la apk

--- lib/features/auth/presentation/blocs/register_cubit/register_state.dart
+++ lib/features/auth/presentation/blocs/register_cubit/register_state.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
 import 'package:migozz_app/features/auth/models/user_dto.dart';
 import 'package:migozz_app/features/auth/models/location_dto.dart';
 
