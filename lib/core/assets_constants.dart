012eb29b0d85948d640c7edbf4e83542d953e03e
JuanEs Arenilla Buendia <juanes.arenilla@gmail.com>
2025-12-10 19:31:16 -0500

    add fb icon and linkedin icon

--- lib/core/assets_constants.dart
+++ lib/core/assets_constants.dart
@@ -16,15 +16,14 @@ class AssetsConstants {
       '$socialBlackWhiteIcons/youtube_black.svg';
 
   static const String tiktokIcon = '$socialBlackWhiteIcons/TiktokIconNegro.svg';
-  static const String facebookIcon = '$socialBlackWhiteIcons/DiscordNegro.svg';
+  static const String facebookIcon = '$socialBlackWhiteIcons/FbNegro.svg';
   static const String telegramIcon = '$socialBlackWhiteIcons/TelegramNegro.svg';
   static const String whatsappIcon = '$socialBlackWhiteIcons/WhatsappNegro.svg';
   static const String pinterestIcon =
       '$socialBlackWhiteIcons/PinterestNegro.svg';
   static const String spotifyIcon = '$socialBlackWhiteIcons/SpotifyNegro.svg';
   static const String twitterIcon = '$socialBlackWhiteIcons/TwitterNegro.svg';
-  static const String linkedinIcon =
-      '$socialBlackWhiteIcons/instagram_black.svg';
+  static const String linkedinIcon = '$socialBlackWhiteIcons/LinkedInNe.svg';
 }
 
 // DiscordBlanco.svg
